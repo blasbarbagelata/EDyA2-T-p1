7b45640cca328eb97b50dfc15b455cba062041ad
TomasCastro <tomascastrorojas18@gmail.com>
2021-04-30 15:14:21 -0300

    mergesort para calcular mediana y pattern matching en coord

--- arboles.hs
+++ arboles.hs
@@ -1,9 +1,8 @@
-{-data NdTree p = Node (NdTree p) -- subárbol izquierdo
-                      p -- punto
+data NdTree p = Node (NdTree p)  -- subárbol izquierdo
+                      p          -- punto
                       (NdTree p) -- subárbol derecho
-                      Int -- eje
-                | Empty
-deriving (Eq, Ord, Show)-}
+                      Int        -- eje
+                | Empty deriving (Eq, Ord, Show)
 
 class Punto p where
   dimension :: p -> Int -- devuelve el número de coordenadas de un punto
@@ -16,9 +15,39 @@ newtype Punto3d = P3d (Double, Double, Double)
 
 instance Punto Punto2d  where
   dimension p = 2
-  coord i (P2d p) = if i == 0 then fst p else snd p
+  coord 0 (P2d (x,y)) = x
+  coord 1 (P2d (x,y)) = y
+  --coord _ p = NaN
 
 instance Punto Punto3d where
   dimension p = 3
-  coord i (P3d (x,y,z)) = (if i == 0 then x else (if i == 1 then y else z )) 
+  coord 0 (P3d (x,y,z)) = x
+  coord 1 (P3d (x,y,z)) = y
+  coord 2 (P3d (x,y,z)) = z
+  --coord _ p = NaN
 
+
+{-fromList :: Punto p => [p] -> NdTree p
+fromList [] = Empty
+fromList (p:ps) = -- Eleccion eje = level%dimension p -}
+
+merge :: (Num a, Ord a) => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) = if x <= y then (x:merge xs (y:ys))
+                                else (y:merge (x:xs) ys)
+
+split :: (Num a, Ord a) => [a] -> ([a], [a])
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys) 
+
+msort :: (Num a, Ord a) => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = let (ls, rs) = split xs
+               (ls1, rs1) = (msort ls, msort rs)
+           in merge ls1 rs1
+
+mediana :: Punto p => [p] -> Int -> Double
+mediana ps k = msort [coord k p | p<-ps]
