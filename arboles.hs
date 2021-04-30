6b190e62c9b7262f17f800c64a120b72c5a326ed
TomasCastro <tomascastrorojas18@gmail.com>
2021-04-30 16:15:32 -0300

    signatura p y mediana de una lista de puntos respecto a un eje

--- arboles.hs
+++ arboles.hs
@@ -10,7 +10,7 @@ class Punto p where
   dist :: p -> p -> Double -- calcula la distancia entre dos puntos
   dist p q = sum [(coord i p - coord i q)^2 | i <- [0..(dimension p) - 1]] 
 
-newtype Punto2d = P2d (Double, Double) 
+newtype Punto2d = P2d (Double, Double) deriving Show
 newtype Punto3d = P3d (Double, Double, Double) deriving Show
 
 instance Punto Punto2d  where
@@ -31,23 +31,27 @@ instance Punto Punto3d where
 fromList [] = Empty
 fromList (p:ps) = -- Eleccion eje = level%dimension p -}
 
-mergePunto :: Punto a => Int -> [a] -> [a] -> [a]
+mergePunto :: Punto p => Int -> [p] -> [p] -> [p]
 mergePunto k [] ys = ys
 mergePunto k xs [] = xs
 mergePunto k (x:xs) (y:ys) = if coord k x <= coord k y then (x:mergePunto k xs (y:ys))
                                 else (y:mergePunto k (x:xs) ys)
 
-split :: Punto a => [a] -> ([a], [a])
+split :: Punto p => [p] -> ([p], [p])
 split [] = ([], [])
 split [x] = ([x], [])
 split (x:y:zs) = let (xs, ys) = split zs in (x:xs, y:ys) 
 
-msortPunto :: Punto a => [a] -> Int -> [a]
+msortPunto :: Punto p => [p] -> Int -> [p]
 msortPunto [] k = []
 msortPunto [x] k = [x]
 msortPunto xs k = let (ls, rs) = split xs
                       (ls1, rs1) = (msortPunto ls k, msortPunto rs k)
                   in mergePunto k ls1 rs1
 
-{-mediana :: Punto p => [p] -> Int -> p
-mediana ps k = (msortPunto ps k)-}
+mediana :: Punto p => [p] -> Int -> p
+mediana ps k = let ordps = msortPunto ps k
+                   largo = length ordps in if (mod largo 2) == 0 then ordps!!(div largo 2)
+                                                                 else ordps!!(div (largo-1) 2)
+
+--prueba = mediana [P2d(2,3), P2d(5,4), P2d(9,6), P2d(4,7), P2d(8,1), P2d(7,2)] 0
